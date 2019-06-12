d955b05ecc96aaa6b2462a01db0d52204f4de8ca
Fabricio Catae <fcatae@hotmail.com>
2019-06-11 22:42:18 -0300

    initial commit

---
+++ MeuTrabalho/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MeuTrabalho.Models;
+using System.Data.SqlClient;
+
+namespace MeuTrabalho.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(LoginViewModel model)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Server=saturnoserver.database.windows.net;Database=MEUDB;User=aclogin;Password=homework-jan31");
+                SqlCommand cmd = new SqlCommand($"SELECT username FROM tbLogin WHERE email='{model.Email}' AND pwd='{model.Password}'", connection);
+
+                connection.Open();
+                string username = (string)cmd.ExecuteScalar();
+
+                connection.Close();
+
+                return RedirectToAction("Dashboard", "Home", new { name = username});
+            }
+            catch(Exception ex)
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ MeuTrabalho/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MeuTrabalho.Models;
+using System.Data.SqlClient;
+
+namespace MeuTrabalho.Controllers
+{
+    public class HomeController : Controller, IDisposable
+    {
+        SqlConnection connection;
+
+        public HomeController()
+        {
+            this.connection = new SqlConnection("Server=saturnoserver.database.windows.net;Database=MEUDB;User=app;Password=homework-jan31;Max Pool Size=10");
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToActionPermanent("Index", "Account");
+        }
+
+        public IActionResult Dashboard(string name)
+        {
+            if( name == null )
+            {
+                throw new ArgumentNullException(name);
+            }
+
+            ViewBag.Name = name;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            try
+            {
+                this.connection.Open();
+
+                SqlCommand sql = new SqlCommand("INSERT tbLog VALUES ('about')", this.connection);
+                sql.ExecuteReader();                
+            }
+            catch(Exception ex)
+            {
+                ViewData["Message"] = "ERROR ABOUT";
+            }
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            try
+            {
+                SqlConnection conn1 = this.connection;
+
+                SqlCommand sql = new SqlCommand("INSERT tbLog VALUES ('contact')");
+                sql.Connection = conn1;
+
+                sql.ExecuteScalar();
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
